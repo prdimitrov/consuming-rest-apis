9454e1bb520c3e0152aa4cfaac793820107f56ec
prdimitrov <prdimitrov97@gmail.com>
2024-09-16 17:00:40 +0300

    - Initial commit.

---
+++ src/main/java/com/digitalrazgrad/consuming_rest_apis/ConsumingRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.digitalrazgrad.consuming_rest_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalrazgrad/consuming_rest_apis/ConsumingRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalrazgrad.consuming_rest_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
